747ebc9fd6b6e49acddb97c6a9735a71783bdd3a
Daniel <daniel3levi@gmail.com>
2022-12-01 17:55:05 +0200

    Start new leet code problem

--- valid-word-abbreviation/valid-word-abbreviation.js
+++ valid-word-abbreviation/valid-word-abbreviation.js
@@ -5,4 +5,29 @@
  * @param {string} abbr
  * @return {boolean}
  */
-var validWordAbbreviation = function (word, abbr) {};
+var validWordAbbreviation = function (word, abbr) {
+  let abbr_arr = abbr.match(/[a-z]+|[0-9]+/g);
+  console.log(abbr_arr);
+  let j = 0;
+  for (let i = 0; i < word.length; i++) {
+    let current_abbr;
+    if (!isNaN(abbr_arr[j])) {
+      i += Number(abbr_arr[j]);
+      j++;
+      current_abbr = abbr_arr[j];
+      if (!current_abbr[0] === word[i]) {
+        return false;
+      }
+    } else {
+      current_abbr_length = current_abbr.length;
+      j++;
+    }
+  }
+
+  return true;
+};
+
+console.log(validWordAbbreviation('applepie', '3le2e'));
+//validWordAbbreviation('internationalization', 'i12iz4n');
+
+//validWordAbbreviation('substitution', 'sub4u4');
